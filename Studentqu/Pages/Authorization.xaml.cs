020bb2d1cb9bfba376bd04db6cea3d21eb8d26b2
Владислав Давыдов <vlad5803964@yandex.ru>
2025-01-14 15:34:13 +0300

    Добавьте файлы проекта.

---
+++ Studentqu/Pages/Authorization.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Studentqu.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Page
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+        private void ButtonEnter_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TextBoxLogin.Text) || string.IsNullOrEmpty(PasswordBox.Password))
+            {
+                MessageBox.Show("Введите логин или пароль!");
+                return;
+            }
+            using (var db = new Entities())
+            {
+                var user = db.User
+                    .AsNoTracking()
+                    .FirstOrDefault(u => u.Login == TextBoxLogin.Text && u.Password == PasswordBox.Password);
+                if (user == null)
+                {
+                    MessageBox.Show("Пользователь не найден");
+                    return;
+                }
+
+                MessageBox.Show("Пользователь найден");
+
+                switch (user.Role)
+                {
+                    case "Преподователь":
+                        NavigationService.Navigate(new TeacherPage());
+                        break;
+                    case "Студент":
+                        NavigationService.Navigate(new StudentPage());
+                        break;
+
+                }
+            }
+        }
+
+        private void ButtonRegister_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new RegPage());
+        }
+
+    }
+}
